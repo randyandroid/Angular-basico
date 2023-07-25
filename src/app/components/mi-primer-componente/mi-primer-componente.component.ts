873f6ef5d577b3d0f56da0d4b3411aeee6619ef2
Randy <rndmarmol@gmail.com>
2023-07-24 23:12:20 -0400

    Componentes

--- src/app/components/mi-primer-componente/mi-primer-componente.component.ts
+++ src/app/components/mi-primer-componente/mi-primer-componente.component.ts
@@ -16,9 +16,23 @@ template: `
 //creo la clase
 export class miprimercomponente{
 
+    public titulo: string;
+    public mensaje: string;
+    public year: number;
+
+
+    //defino constructor
     constructor(){
+        this.titulo = "Mi primer componente";
+        this.mensaje = "Este es el primer componente";
+        this.year = new Date().getFullYear();
+
     
         console.log("Hola desde mi primer componente");
-        }
+        console.log(this.mensaje,this.titulo,this.year);
+
+        }//fin del constructor
+
+    
 
 }//fin de la clase
